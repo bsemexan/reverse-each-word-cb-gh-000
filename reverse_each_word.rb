b19ab7a202c39ddf2cc2284259e79f1ce6ba3d5e
Berry  Semexan <bsemexan@gmail.com>
2018-02-14 03:11:38 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(reversed)
-  reversed.split(" ")
-  reversed.collect do |word|
+  array = Array.new
+  array = reversed.split(" ")
+  array.collect do |word|
     word.reverse
   end
-  reversed.join(" ")
+  array.join(" ")
 end
